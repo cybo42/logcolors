bfa3113772699f8840ee778978e2857d4feb50cb
Joe Cyboski <cybo42@gmail.com>
2011-04-26 11:38:58 -0400

    cleaned up formatting (2 spaces instead of tabs / 4space)

--- logcolors.pl
+++ logcolors.pl
@@ -17,28 +17,28 @@ if($stdin){
 
 }else{
 
-    if($tail){
-        open(LOG, "tail -f $filename|") || die "Could not tail $filename: $!\n";
-    }else{
-        open(LOG, "$filename") || die "Could not open $filename: $!\n";
-    }
+  if($tail){
+     open(LOG, "tail -f $filename|") || die "Could not tail $filename: $!\n";
 
+  }else{
+     open(LOG, "$filename") || die "Could not open $filename: $!\n";
+  }
 }
 
-
 while($line = <LOG>){
-    my $matched = 0;
-    foreach $regex (keys(%$colorMap)){
-        if($line =~ /$regex/){
-            print colored($line, $colorMap->{$regex});
-            $matched = 1;
-            last;
-        }
-    }
+  my $matched = 0;
 
-    unless($matched){
-      print $line;
+  foreach $regex (keys(%$colorMap)){
+    if($line =~ /$regex/){
+      print colored($line, $colorMap->{$regex});
+      $matched = 1;
+      last;
     }
+  }
+
+  unless($matched){
+    print $line;
+  }
 }
 
 sub resetAndExit{
@@ -51,13 +51,13 @@ sub loadColorMap{
   my $colorMap = {};
 
   if(-e $colorFile){
-      $colorMap = parseColorFile($colorFile);
+    $colorMap = parseColorFile($colorFile);
 
   }else{
-      $colorMap->{'ERROR'} = "red";
-      $colorMap->{'Error'} = "red";
-      $colorMap->{'DEBUG'} = "cyan";
-      $colorMap->{'WARN'} = "yellow";
+    $colorMap->{'ERROR'} = "red";
+    $colorMap->{'Error'} = "red";
+    $colorMap->{'DEBUG'} = "cyan";
+    $colorMap->{'WARN'} = "yellow";
   }
 
   return $colorMap;
